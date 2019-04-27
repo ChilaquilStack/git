4ef8be91a22e1b6ec976bfe5d6f9de90825087be
Gizmo0oo <contact@eddivillegas.com>
2019-04-27 16:37:15 -0500

    feature: Listas
    Se agregaron listas ordenas y no ordenadas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 "use strict"
 
-const showMessage = (message) => alert(message);
+const showMessageByConsole (message) => console.log(message); 
+const showMessageByAlert = (message) => alert(message);
 const start = () => showMessage('Bien Benido');
 
 window.addEventListener('load', start);
